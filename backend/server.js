1f690252425ab6bcd9581a038c31491288dea64f
Vishmi Navanjana <127643853+NavanjanaLAV@users.noreply.github.com>
2024-03-17 01:16:43 +0530

    connected userDB

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleWare/errorMiddleware")
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
+// Routes Middleware
+app.use("/api/users", userRoute);
+
+//Routes
+app.get("/", (req,res) =>{
+    res.send("Home Page");
+});
+
+const PORT = process.env.PORT || 5000;
+// Error Middleware
+app.use(errorHandler);
 // Connect to DB and start server
 mongoose
         .connect(process.env.MONGO_URI)
         .then(() => {
             app.listen(PORT,() =>{
-                console.log('Server Running on port ${PORT}')
+                console.log(`Server Running on port ${PORT}`)
             })
 
         })
