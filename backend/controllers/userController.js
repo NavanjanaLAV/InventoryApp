0ea30abfbd154a2790f917fcda3ab5f1204a1fb5
Vishmi Navanjana <127643853+NavanjanaLAV@users.noreply.github.com>
2024-03-20 02:26:46 +0530

    connect to the email

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,20 +43,6 @@ const registerUser = asyncHandler(async (req, res) => {
         password,
 
     })
-    
-     // Generate Token
-     const token = generateToken(user._id);
-
-     // Send HTTP-only cookie
-     res.cookie("token", token, {
-        path: "/",
-        httpOnly: true,
-        expires: new Date(Date.now() + 1000 * 86400), //1 day
-        sameSite: "none",
-        secure: true
-
-
-     });
 
     if(user){
         const {_id, name, email, position, phone} = user
@@ -78,7 +64,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
 // Login User
-const loginUser = asyncHandler (async(req, res) => {
+const loginUser = asyncHandler(async(req, res) => {
     const {email,password} = req.body
 
     //validate Request
@@ -97,16 +83,31 @@ const loginUser = asyncHandler (async(req, res) => {
     }
 
     //User exists, check if password is correct
-    const passwordIsCorrect = await bcrypt.compare(password, user.password)
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+    // Generate Token
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+       path: "/",
+       httpOnly: true,
+       expires: new Date(Date.now() + 1000 * 86400), //1 day
+       sameSite: "none",
+       secure: true,
+
+
+    });
 
     if(user && passwordIsCorrect){
-        const {_id, name, email, position, phone} = user
+        const {_id, name, email, position, phone} = user;
         res.status(200).json({
             _id, 
             name, 
             email, 
             position, 
             phone,
+            token,
         });
     } else{
         res.status(400);
@@ -118,9 +119,92 @@ const loginUser = asyncHandler (async(req, res) => {
 
 });
 
+//Logout User
+const logout = asyncHandler(async(req, res) => {
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(0), 
+        sameSite: "none",
+        secure: true,
+ 
+ 
+     });
+     return res.status(200).json({ message: "Successfully Logged Out"})
+
+});
+
+//Get User Data
+const getUser =asyncHandler(async(req, res) =>{
+    const user = await User.findById(req.user._id)
+
+    if(user){
+        const {_id, name, email, position, phone} = user;
+        res.status(200).json({
+            _id, 
+            name, 
+            email, 
+            position, 
+            phone,
+        });
+    } else{
+        res.status(400);
+        throw new Error("User Not Found");
+    }
+});
+
+//Get Login Status
+const loginStatus = asyncHandler(async(req, res) => {
+
+    const token = req.cookies.token;
+    if (!token){
+        return res.json(false);
+    }
+    //Verify Token
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if(verified){
+        return res.json(true);
+
+    }
+    return res.json(false);
+    
+});
+
+// Update User
+ const updateUser = asyncHandler (async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user){
+        const {name, email, position, phone} = user;
+        user.email = email;
+        user.name = req.body.name || name;
+        user.phone = req.body.phone || phone;
+        user.position = req.body.position || position;
+
+        const updateUser = await user.save()
+        res.status(200).json({
+            _id: updateUser._id, 
+            name: updateUser.name, 
+            email: updateUser.email, 
+            position: updateUser.position, 
+            phone: updateUser.phone,
+
+        })
+
+    }else {
+        res.status(404)
+        throw new Error("User not found")
+
+    }
+ });
 
 module.exports = {
     registerUser,
     loginUser,
+    logout,
+    getUser,
+    loginStatus,
+    updateUser
+
 };
 
